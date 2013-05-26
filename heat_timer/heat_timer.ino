e5c7941eba331c6065e1af0777311b13b12e3f15
German Borisov <Borisov.German@gmail.com>
2013-05-26 23:47:55 +0400

    heat_timer Подсчет минут и секунд

--- heat_timer/heat_timer.ino
+++ heat_timer/heat_timer.ino
@@ -3,8 +3,8 @@
 // Блок описания пинов
 #define dot_pin    0 // точка для индикации секунд
 #define enc_2_pin  1 // энкодер 2
-#define enc_1_pin  2 // энкодер 1
-#define button_pin 3 // кнопка int1
+#define enc_1_pin  2 // энкодер 1 (int0)
+#define button_pin 3 // кнопка (int1)
 #define led_1_a    4 // инд.1 A
 #define led_1_b    5 // инд.1 B
 #define led_1_c    6 // инд.1 C
@@ -22,34 +22,37 @@
 #define led_2_f   A4 // инд.2 F
 #define led_2_g   A5 // инд.2 G
 
+#define max_min 90
+
 SSI D1(led_1_a, led_1_b, led_1_c, led_1_d, led_1_e, led_1_f, led_1_g);
 SSI D2(led_2_a, led_2_b, led_2_c, led_2_d, led_2_e, led_2_f, led_2_g);
 
-volatile uint8_t count;
+volatile uint8_t count_min;
+volatile uint8_t count_sec;
 volatile unsigned long curr_millis;
 volatile unsigned long last_millis;
 
 void setup() {
-  // put your setup code here, to run once:
   pinMode(dot_pin, OUTPUT);
-//  pinMode(relay_pin, OUTPUT); ???
+  pinMode(relay_pin, INPUT);
   pinMode(enc_1_pin, INPUT);
   pinMode(enc_2_pin, INPUT);
   pinMode(button_pin, INPUT);
-//  pinMode(therm_pin, INPUT);
+  //pinMode(therm_pin, INPUT);
   digitalWrite(dot_pin, LOW);
-    count=0;
-  last_millis=0;
+  count_min = 0;
+  count_sec = 0;
+  last_millis = 0;
   attachInterrupt(0, enc_int, RISING); //enc_1_pin
-  attachInterrupt(1, button_int, RISING); //enc_1_pin
+  attachInterrupt(1, button_int, RISING); //button_pin
 }
 
 void displayNumber(byte number) {
-  if (number<10)
+  if (number < 10)
     D1.off();
   else
-    D1.setDigit(number/10);
-  D2.setDigit(number%10);
+    D1.setDigit(number / 10);
+  D2.setDigit(number % 10);
 }
 
 void displayEr(void) {
@@ -73,35 +76,45 @@ void displayOff(void) {
 }
 
 void loop() {
-  if(last_millis && count) {
+  if(last_millis && count_min) {
     curr_millis = millis();
     if(curr_millis - last_millis > 1000) {
-      --count;
-      if (count)
-        last_millis+=1000;
+      --count_sec;
+      if (!count_sec) {
+        --count_min;
+        count_sec = 60;
+      }
+      if (count_min)
+        last_millis += 1000;
       else
-        last_millis=0;  
-    }    
+        last_millis = 0;  
+    }
+    digitalWrite(dot_pin, count_sec & 1);
   }  
-  displayNumber(count);
-  digitalWrite(relay_pin, (count==0?LOW:HIGH));
+  displayNumber(count_min);
+  digitalWrite(relay_pin, (count_min == 0 ? LOW : HIGH));
 }
 
 void enc_int() {
-  if (digitalRead(enc_2_pin)==HIGH)
-  {
-    if(count<99)
-      ++count;
-  }
-  else
-  {
-    if(count>0)
-      --count;
+  if (digitalRead(enc_2_pin) == HIGH) {
+    if(count_min < max_min)
+      ++count_min;
+    count_sec = 60;
+  } 
+  else {
+    if(count_min > 0) {
+      --count_min;
+      count_sec = 60;
+    } 
+    else
+      count_sec = 0;
   }
   last_millis = millis();
 }
 
 void button_int() {
-  count = 0;
+  count_min = 0;
+  count_sec = 0;
   last_millis = 0;
 }
+
